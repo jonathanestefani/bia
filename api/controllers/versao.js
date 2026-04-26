1f671720fdd9502c8ea6df89877ed12227b98b05
Jonathan Estefani <jonathan.estefani@gmail.com>
2026-04-26 23:22:09 +0000

    alteracao da versao

--- api/controllers/versao.js
+++ api/controllers/versao.js
@@ -2,7 +2,7 @@ module.exports = () => {
   const controller = {};
 
   controller.get = async (req, res) => {
-    const responseString = `Bia ${process.env.VERSAO_API || "4.2.2"}`;
+    const responseString = `Bia ${process.env.VERSAO_API || "4.2.3"}`;
     res.send(responseString);
   };
 
